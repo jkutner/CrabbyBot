1561830774e6f855672f4ce006b33116740703a0
Greyden <greyden.kutner@gmail.com>
2020-04-02 22:16:47 -0500

    handle mentions

--- bot.py
+++ bot.py
@@ -10,6 +10,14 @@ slack_events_adapter = SlackEventAdapter(slack_signing_secret, "/slack/events")
 token = os.environ["SLACK_TOKEN"]
 slack_client = slack.WebClient(token=token)
 
+@slack_events_adapter.on("app_mention")
+def handle_mention(event_data):
+    event = event_data["event"]
+    channel = event["channel"]
+    user = event["user"]
+    message = "<@%s> :zipper_mouth_face:" % (user)
+    slack_client.chat_postMessage(channel=channel, text=message)
+
 # Once we have our event listeners configured, we can start the
 # Flask server with the default `/events` endpoint on port 3000
 PORT = os.environ["PORT"]
